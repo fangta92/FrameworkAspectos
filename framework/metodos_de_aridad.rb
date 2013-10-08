30ccecf314a91aaa4148d45c7f2bd8bcaf055555
David <fangta92@hotmail.com>
2013-10-07 21:30:09 -0300

    corrijo metodos_que_cumplen porque arity no trabaja con simbolos

--- framework/metodos_de_aridad.rb
+++ framework/metodos_de_aridad.rb
@@ -1,11 +1,16 @@
 class MetodosDeAridad < JoinPoint
 
-    def initialize(aridad)
-      @aridad = aridad
-    end
+  def initialize(aridad=2)
+    @aridad = aridad
+  end
 
-    def metodos_que_cumplen
-      metodos_de_las_clases.select do |clase_metodo| clase_metodo.metodo.arity == @aridad end
-    end
+  def metodos_que_cumplen
+    metodos_de_las_clases.select do |clase_metodo| clase_metodo.clase.instance_method(clase_metodo.metodo).arity == @aridad end
+  end
+
+
+#caso de prueba
+  def metodo_de_aridad_5(a,s,d,f,g)
+  end
 
 end

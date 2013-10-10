fc80a6291668cf980b5fb54bd95f8811d411ed52
Flor <florencia.vojnov@hotmail.com>
2013-10-10 13:01:19 -0300

    Agrego el punto de los metodos accessors y su respectivo test

---
+++ framework/metodos_accessors.rb
@@ -0,0 +1,15 @@
+class MetodosAccessors < JoinPoint
+
+  def interesa?(clase_metodo)
+      getter?(clase_metodo) || setter?(clase_metodo)
+  end
+
+  def setter?(clase_metodo)
+    /=/.match clase_metodo.metodo.to_s
+  end
+
+  def getter?(clase_metodo)
+    (clase_metodo.clase.instance_methods(false)).include? ((clase_metodo.metodo.to_s + "=")).to_sym
+  end
+
+end
--- test/clases_de_prueba.rb
+++ test/clases_de_prueba.rb
@@ -12,7 +12,7 @@ class B < A
 end
 
 class C < A
-  def ccc(opcional=2, requerido)
+  def ccc(requerido, opcional=2)
 
   end
 end
@@ -26,4 +26,13 @@ end
 class Aridad5
   def metodo_de_aridad_5(a,s,d,f,g)
   end
+end
+
+class Persona
+  attr_accessor :nombre
+
+  def cualquiera
+
+  end
+
 end

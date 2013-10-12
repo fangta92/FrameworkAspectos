da01ffd2107a83366811ce60887f567f6810f0af
pabloferro <pablo.pyp@gmail.com>
2013-10-12 16:23:02 -0300

    Renombro clasePadre a clase_padre

--- framework/jerarquia_de_clases.rb
+++ framework/jerarquia_de_clases.rb
@@ -1,9 +1,9 @@
 class JerarquiaDeClases < JoinPoint
-  def initialize(clasePadre)
-    @clasePadre = clasePadre
+  def initialize(clase_padre)
+    @clase_padre = clase_padre
   end
 
   def interesa? (clase_metodo)
-    clase_metodo.clase <= @clasePadre
+    clase_metodo.clase <= @clase_padre
   end
 end
